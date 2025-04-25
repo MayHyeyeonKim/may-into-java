0163fcd550ece908bab88a2d3f7cfceab29d8e6b
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-24 23:03:54 -0700

    ._.

---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
+// 자바에서는 모든 코드는 클래스 안에 있어야 하기 때문에, 함수 단독으로는 실행이 안 됨.
+// 즉, Java는 "클래스 기반 언어"이기 때문
---
+++ java-start/src/HelloJava2.java
@@ -0,0 +1,21 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+        main2(args);
+    }
+
+    public static void main2(String[] args) {
+        System.out.println("H1");
+        System.out.println("H2");
+    }
+}
+
+class HelloJava3 {
+    public static void main(String[] args) {
+        System.out.println("H1");
+        System.out.println("H2");
+    }
+}
