480df670d7ee48d95b1a346501dbe822d4996d37
MayHyeyeonKim <hkim7963@gmail.com>
2025-04-24 23:19:59 -0700

    basic

---
+++ java-basic/src/class1/ClassStart1.java
@@ -0,0 +1,17 @@
+package class1;
+
+public class ClassStart1 {
+
+    public static void main(String[] args) {
+        String student1Name = "MayChu";
+        int student1Age = 20;
+        int student1Grade = 1000;
+
+        String student2Name = "ChuChu";
+        int student2Age = 30;
+        int student2Grade = 1500;
+
+        System.out.println("이름:" + student1Name + " 나이:" + student1Age + " 성적:" + student1Grade);
+        System.out.println("이름:" + student2Name + " 나이:" + student2Age + " 성적:" + student2Grade);
+    }
+}
